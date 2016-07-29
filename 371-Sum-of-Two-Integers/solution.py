b399274c0e8421d6b38a9ccff08d496a33ff2c6d
limingjun <relap@users.noreply.github.com>
2016-07-29 21:03:19 +0800

    [371. Sum of Two Integers][Accepted]committed by LeetCode Extension

--- 371-Sum-of-Two-Integers/solution.py
+++ 371-Sum-of-Two-Integers/solution.py
@@ -5,10 +5,12 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
+        MOD     = 0xFFFFFFFF
+        MAX_INT = 0x7FFFFFFF
         while b!=0:
-            c = a ^ b
-            d = (a & b) << 1
+            c = (a ^ b) & MOD
+            d = ((a & b) << 1) & MOD
             a = c
             b = d
         else:
-        	return a
+        	return a if a <= MAX_INT else ~(a & MAX_INT) ^ MAX_INT
