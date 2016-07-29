54f248a9b160ff6abecdeb2caf8e59f9372c5371
limingjun <relap@users.noreply.github.com>
2016-07-29 22:03:19 +0800

    [326. Power of Three][Time Limit Exceeded]

--- 326-Power-of-Three/solution.py
+++ 326-Power-of-Three/solution.py
@@ -4,7 +4,10 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if n in [3**x for x in range(1000) if 3**x <= n]:
-        	return True
-        else:
+        if n%10 not in [1,3,7,9]:
         	return False
+        else:
+	        if n in [3**x for x in range(1000) if 3**x <= n]:
+	        	return True
+	        else:
+	        	return False
