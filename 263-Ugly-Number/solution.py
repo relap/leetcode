f80cf55a2d45d31810914144b8de17bae886764e
limingjun <relap@users.noreply.github.com>
2016-08-01 22:53:19 +0800

    [263. Ugly Number][Accepted]

--- 263-Ugly-Number/solution.py
+++ 263-Ugly-Number/solution.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
+        if num <= 0:
+        	return False 
         while num%2 == 0:
         	num /= 2
         while num%3 == 0:
