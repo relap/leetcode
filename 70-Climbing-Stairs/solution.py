e99b47102fe041b9cd466d06ba2745737b49d7e9
limingjun <relap@users.noreply.github.com>
2016-08-06 00:11:35 +0800

    [70. Climbing Stairs][Time Limit Exceeded]

---
+++ 70-Climbing-Stairs/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        if n >= 3:
+            return self.climbStairs(n-1)+self.climbStairs(n-2)
