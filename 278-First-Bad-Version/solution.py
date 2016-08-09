b526122b9cf0e1d1cb9393bab9d588ea3dc0437b
limingjun <relap@users.noreply.github.com>
2016-08-09 20:30:01 +0800

    [278. First Bad Version][Runtime Error]

---
+++ 278-First-Bad-Version/solution.py
@@ -0,0 +1,23 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        minn = 0
+        maxn = n - 1
+        mid = (minn + maxn) /2
+        while !(isBadVersion(mid) == True and isBadVersion(mid-1) == False):
+        	if isBadVersion(mid) == True:
+        		maxn = mid
+        		mid = (minn + maxn) /2
+        	else:
+        		minn = mid
+        		mid = (minn + maxn) /2
+        return mid
+        
