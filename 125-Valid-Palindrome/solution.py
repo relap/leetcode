bdf3965bddbb0c5bb1edd058e83fa8fceabfc2e0
limingjun <relap@users.noreply.github.com>
2016-08-10 22:08:05 +0800

    [125. Valid Palindrome][Time Limit Exceeded]

--- 125-Valid-Palindrome/solution.py
+++ 125-Valid-Palindrome/solution.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        
-        if s == '':
-            return True
-        else:
-            sTmp = ''
-            for i in range(0, len(s)):
-                if s[i] >= 'a' and s[i] <= 'z' or s[i] >= '0' and s[i] <= '9' or s[i] >= 'A' and s[i] <= 'Z':
-                    sTmp += s[i]
-            sTmp = sTmp.lower()
-            for i in range(0, len(sTmp)/2):
-                if sTmp[i] != sTmp[len(sTmp)-1-i]:
-                    return False
-            return True
+        sl = s.lower()
+        sa = ''
+        for i in sl:
+        	if i>='a' and i<='z' or i>='0' and i<='9':
+        		sa += i
+        strlen = len(sa)
+
+        for i in range(0,strlen/2):
+        	if sa[i] != sa[strlen-1-i]:
+        		return False
+        return True
